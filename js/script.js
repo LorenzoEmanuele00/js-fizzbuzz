606f1c2269ed2233336303f7ad6a2ec42638d4c4
LorenzoEmanuele00 <lorenzoema2800@gmail.com>
2023-10-20 17:50:34 +0200

    restyling

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
+let grid = "";
+
 for (let i = 1; i <= 100; i++) {
-    let result = "";
+    let result;
     if (i % 3 === 0 && i % 5 === 0) {
-        result = "fizzbuzz";
+        result = "FIZZBUZZ";
+        grid += `<div class = "p-1"> <div class="card d-flex justify-content-center align-items-center fizzbuzz ms_dim"> ${result} </div> </div>`
     } else if (i % 5 == 0) {
-        result = "buzz";
+        result = "BUZZ";
+        grid += `<div class = "p-1"> <div class="card d-flex justify-content-center align-items-center buzz ms_dim"> ${result} </div> </div>`
     } else if (i % 3 === 0) {
-        result = "fizz"
+        result = "FIZZ"
+        grid += `<div class = "p-1"> <div class="card d-flex justify-content-center align-items-center fizz ms_dim"> ${result} </div> </div>`
+    } else {
+        grid += `<div class = "p-1"> <div class="card d-flex justify-content-center align-items-center natural ms_dim"> ${i} </div> </div>`
     }
     console.log (i, result)
 }
+
+document.querySelector(".row").innerHTML = grid;
